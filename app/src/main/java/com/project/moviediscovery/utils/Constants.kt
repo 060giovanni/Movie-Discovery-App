47c47d576939849966cf9c88e4b2935a7d2c835e
121140060_Giovanni Lucy <92906302+Giovanni121140060@users.noreply.github.com>
2024-05-16 00:36:59 +0700

    finished project

--- app/src/main/java/com/project/moviediscovery/utils/Constants.kt
+++ app/src/main/java/com/project/moviediscovery/utils/Constants.kt
@@ -18,7 +18,7 @@ object Constants {
             show()
         }
     }
-
+    
     const val BASE_POSTER_PATH = "https://image.tmdb.org/t/p/w342"
     const val BASE_BACKDROP_PATH = "https://image.tmdb.org/t/p/w780"
 }
